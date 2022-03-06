b6bd1c4bb4b00a2f1e5713ab00e850b6eeb4dc85
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-03-06 11:12:14 -0500

    commit tree 2eca489a72d11c14b763cfa4085cec76003aeb59

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(str) {
+    return str.replaceAll(/[^A-z0-9' -]|\^/g, '')
+  }
+  static titleize(string){
+    const words = string.split(" ");
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const result = []
+    for (let i = 0; i < words.length; i++) {
+      if(i == 0) {
+       result.push(this.capitalize(words[i]))
+           } else {if(exceptions.includes(words[i])) {
+             result.push(words[i])
+           } else {
+            result.push(this.capitalize(words[i]))
+           } 
+           }
+      } return result.join(' ')
+
+  }
 }
